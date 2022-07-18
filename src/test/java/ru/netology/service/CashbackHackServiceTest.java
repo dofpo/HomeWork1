ae5077a7dcee7c157013a594aa1768d7e21a528b
dofpo <z-salawat91@mail.ru>
2022-07-18 18:09:56 +0500

    initial junit4 commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,10 +7,18 @@ import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
-    public void remain() {
-        CashbackHackService service= new CashbackHackService();
-        int actual= service.remain(1000);
-        int expected=1000;
-        assertArrayEquals(expected,actual);
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 2000;
+        int actual = service.remain(1000);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainEqualsAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1000;
+        int actual = service.remain(1000);
+        assertEquals(expected, actual);
     }
 }
